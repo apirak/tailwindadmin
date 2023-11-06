8ae053e87702c7cc2313f9e76988d913d5f09966
Apirak Panatkool <apirakb@gmail.com>
2023-11-07 00:38:19 +0700

    js lib

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,6 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "alpinejs", to: "https://ga.jspm.io/npm:alpinejs@3.13.2/dist/module.esm.js"
+pin "@alpinejs/persist", to: "https://ga.jspm.io/npm:@alpinejs/persist@3.13.2/dist/module.esm.js"
+pin "flatpickr", to: "https://ga.jspm.io/npm:flatpickr@4.6.13/dist/esm/index.js"
