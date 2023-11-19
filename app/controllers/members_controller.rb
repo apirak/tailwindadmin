ed17d23693c7476ee7f12c35ed3ff413265e0253
Apirak Panatkool <apirakb@gmail.com>
2023-11-19 18:05:24 +0700

    Add error for wrong invitation

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,9 +5,40 @@ class MembersController < ApplicationController
     @members = @current_team.members
   end
 
+  def invite
+    email = params[:email]
+    return redirect_with_alert('No email provided') if email.blank?
+
+    user = find_or_invite_user(email)
+    return redirect_with_alert('Email invalid') unless user.valid?
+
+    # TODO: Email that user has been added to this team
+
+    add_user_to_team(user)
+    redirect_to team_members_path(@current_team), notice: "#{email} invited!"
+  end
+
   private
 
   def set_current_team
     @current_team = Team.find(params[:team_id])
+  end
+
+  def redirect_with_alert(message)
+    redirect_to team_members_path(@current_team), alert: message
+  end
+
+  def find_or_invite_user(email)
+    User.find_by(email: email) || User.invite!({ email: email }, current_user)
+  end
+
+  def add_user_to_team(user)
+    user.members.find_or_create_by(
+      team: @current_team,
+      roles: {
+        admin: false,
+        editor: true
+      }
+    )
   end
 end
