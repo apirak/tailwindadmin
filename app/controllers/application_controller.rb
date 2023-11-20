66434e0a3434cc4a3811d46c8a3defa0d5f995ea
Apirak Panatkool <apirakb@gmail.com>
2023-11-20 22:10:08 +0700

    Config sign in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    dashboard_path # replace with your actual dashboard path
+  end
 end
