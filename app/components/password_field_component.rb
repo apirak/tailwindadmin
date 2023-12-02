6aac1e26a5e80b33e2b972608b57fb116ee5f80b
Apirak Panatkool <apirakb@gmail.com>
2023-12-02 22:25:51 +0700

    Add password field

--- app/components/password_field_component.rb
+++ app/components/password_field_component.rb
@@ -3,12 +3,21 @@
 class PasswordFieldComponent < ViewComponent::Base
   def initialize(form:, options: {})
     @f = form
+    @label_text = "Password"
+    @field_name = :password
+    @placeholder = "Enter Password"
+    @autocomplete = "current-password"
     @options = options
-    @label = "Password"
+
     if options.include?(:minimum_password_length)
-      @minimum_password_length = @options[:minimum_password_length]
-      @label =
-        "Password <em>(#{@minimum_password_length} characters minimum)</em>".html_safe
+      @mmp = @options[:minimum_password_length]
+      @label_text = "Password (#{@mmp} characters minimum)"
+    else
+      @label_text = @options[:label_text] if options.include?(:label_text)
     end
+
+    @field_name = @options[:field_name] if options.include?(:field_name)
+    @placeholder = @options[:placeholder] if options.include?(:placeholder)
+    @autocomplete = @options[:autocomplete] if options.include?(:autocomplete)
   end
 end
